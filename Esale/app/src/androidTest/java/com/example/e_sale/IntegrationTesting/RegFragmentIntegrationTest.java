b778b979711aa793ecac3abba9d64c2800048f1e
Aritra Naha Likhan <128929550+AritraLikhan@users.noreply.github.com>
2024-05-21 18:30:04 +0600

    Update RegFragmentIntegrationTest.java

--- Esale/app/src/androidTest/java/com/example/e_sale/IntegrationTesting/RegFragmentIntegrationTest.java
+++ Esale/app/src/androidTest/java/com/example/e_sale/IntegrationTesting/RegFragmentIntegrationTest.java
@@ -29,7 +29,7 @@ public class RegFragmentIntegrationTest {
     public ActivityScenarioRule<MainActivity> activityRule
             = new ActivityScenarioRule<>(MainActivity.class);
 
-    private RegFragment regFragment;
+   
 
     @Before
     public void setUp() {
